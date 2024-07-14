bd2452c6d502b71e6e04cfbad25bf3fb2bf8ab90
Dayvison Lacerda <dayvisonlacerda@hotmail.com>
2024-07-14 03:01:45 +0000

    adicionando arquivos

--- PythonPOO/Decoradores,Iteradores e Geradores/decorar_com_argumentos.py
+++ PythonPOO/Decoradores,Iteradores e Geradores/decorar_com_argumentos.py
@@ -1,19 +1,29 @@
-import os
 
-def meu_decorador(funcao):
+# def meu_decorador(funcao):
+#     def envelope(*aw, **kw):
+#         print("Faz algo antes de executar a função.")
+#         funcao(*aw, **kw)
+#         print("Faz algo depois de executar a função.")
+
+#     return envelope
+
+# @meu_decorador
+# def ola_mundo(nome, idade):
+#     print(f"Olá {nome} com {idade} anos de idade,Bem vindoao Mundo!!")
+
+
+# ola_mundo("Dayvison", 20)
+
+def duplicar(func):
     def envelope(*aw, **kw):
-        print("Faz algo antes de executar a função.")
-        funcao(*aw, **kw)
-        print("Faz algo depois de executar a função.")
-
-    return envelope
-
-@meu_decorador
-def ola_mundo(nome):
-    print(f"Olá {nome} ao Mundo!!")
+        func(*aw, **kw)
+        return func(*aw, **kw)
+    return envelope   
 
 
-ola_mundo("Dayvison")
+def aprender(tecnologia):
+    print(f"Estou aprendendo {tecnologia}.")
+    return tecnologia.upper()
 
-input()
-os.system("clear")
+tecnologia = aprender("Python")
+print(tecnologia)
